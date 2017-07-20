1dc1579d32ea8628a46ecea1cb37043a5c3dbc40
寺下 昇希 <shoki.terashita@RJ-MN-193.local>
2017-07-20 19:27:59 +0900

    first commit

---
+++ app/src/main/java/com/example/shokiterashita/learningmaterial/views/activities/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.shokiterashita.learningmaterial.views.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shokiterashita.learningmaterial.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+    //TODO: J
+}
